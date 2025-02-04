3b8bd1abebea71c35c10a53418b97164a9688d4e
daniboy30 <garciaazamardaniel@gmail.com>
2025-02-03 18:58:59 -0600

    vistas.2

--- client/onlineStore/src/routes/protectedRoute.tsx
+++ client/onlineStore/src/routes/protectedRoute.tsx
@@ -1,5 +1,5 @@
 import {Outlet, Navigate} from "react-router-dom";
-import {useState} from "react";
+/*import {useState} from "react";*/
 import {useAuth} from "../auth/AuthProvider.tsx";
 
 export default function ProtectedRoute(){
